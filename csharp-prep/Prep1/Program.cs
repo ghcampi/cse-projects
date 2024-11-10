c1af0abbe276cb4c9e7314e47f548afc9c542fa8
ghcampi <ghcampi@gmail.com>
2024-11-09 23:19:08 -0300

    change the code like the iconic phrase

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}. ");
     }
 }
