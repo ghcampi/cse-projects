397612982ca05815c4a17d632c19afcc45aa7fff
ghcampi <ghcampi@gmail.com>
2024-11-10 03:29:54 -0300

    guess number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
+    //part 1 of the program
+    /*{
+        Console.WriteLine("What is the magic number?");
+        int magic_num = int.Parse (Console.ReadLine());
+        Console.WriteLine("What is your guess?");
+        int guess_num  = int.Parse(Console.ReadLine());
+        if (guess_num< magic_num)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if(guess_num> magic_num)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guesed it! ");
+        }
+
+    }*/
+    // Part 2 of the program
+
+    /*{
+      Console.Write("What is the magic number?: ");
+      int magic_num = int.Parse (Console. ReadLine());
+      Console.Write("What is your guess number? ");
+      int guess_num = int.Parse(Console.ReadLine());
+      while (guess_num != magic_num)
+      {
+        if (guess_num> magic_num)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess_num< magic_num)
+        {
+            Console.WriteLine("Higher" );
+        }
+        
+            Console.Write("What is your guess number? ");
+            guess_num = int.Parse(Console.ReadLine());
+      }
+        Console.WriteLine("You guessed it!");
+    } */
+// Part 3 of the program
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+     Random random_numb = new Random();
+     int magic_num = random_numb. Next(1,101); 
+      Console.Write("What is your guess number? ");
+      int guess_num = int.Parse(Console.ReadLine());
+      while (guess_num != magic_num)
+      {
+        if (guess_num> magic_num)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess_num< magic_num)
+        {
+            Console.WriteLine("Higher" );
+        }
+        
+            Console.Write("What is your guess number? ");
+            guess_num = int.Parse(Console.ReadLine());
+      }
+        Console.WriteLine("You guessed it!");
+    } 
+
 }
+
+   
