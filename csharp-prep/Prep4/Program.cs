eb2de0ec6ccb2b7bfc4e062f22e89e51cc9e3d2c
ghcampi <ghcampi@gmail.com>
2024-11-11 23:27:21 -0300

    Working with list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int inputUser = -1;
+        Console.WriteLine("Enter a list of numbers, tipe 0 when finished.");
+        while (inputUser != 0)
+        {
+            Console.Write("Enter a number ");
+
+            string responseUser = Console.ReadLine();
+            inputUser = int.Parse(responseUser);
+            if(inputUser !=0)
+            {
+                numbers.Add(inputUser);
+            }  
+
+        }
+        //sum of the list.
+        int sum_numbers = 0;
+        foreach (int number in numbers)
+        {
+            sum_numbers += number;
+           
+        }
+        Console.WriteLine($"The sum is: {sum_numbers} ");
+        // Average
+        float average = ((float)sum_numbers)/ numbers.Count;
+        Console.WriteLine($"The average is:{average} ");
+        // Largest number
+        int largest = numbers [0];
+        foreach (int number in numbers)
+        {
+            if(number > largest)
+            {
+                largest = number;
+            }
+        }
+ Console.WriteLine($"The largest number is: {largest}");
+
+
+
     }
+
 }
